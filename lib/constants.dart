aecc00da15ae5a9c05f3765f277bda8a7b58063c
irfansafar <007@Irfan>
2023-09-11 19:03:13 +0530

    initial commit_1

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+
+// colors
+const kBackgroundColor = Color(0xff0a0d22);
+const kActiveColor = Color(0xff111428);
+const kButtonColor = Color(0xffeb1555);
+const kButtonSecondaryColor = Color(0xff1c1f32);
+const kResultColor = Color(0xff22e67b);
+
